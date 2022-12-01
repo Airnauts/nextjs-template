c024d7b3521e1f042bd6b0f72faa24a2ff291a40
Damian Nowakowski <damian@airnauts.com>
2022-12-01 18:12:38 +0100

    WIP: Add Airnauts Style Guide

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+const { resolve } = require('path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@airnauts/style-guide/eslint/typescript'),
+    require.resolve('@airnauts/style-guide/eslint/react-typescript'),
+  ],
+  parserOptions: {
+    project,
+  },
+  parserOptions: {
+    ecmaVersion: "latest"
+  },
+  env: {
+    es6: true
+  }
+};
