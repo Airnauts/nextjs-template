b2ee3635e477d16df19fac9b7d2a79497ba7ebb3
Damian Nowakowski <damian@airnauts.com>
2023-04-18 14:15:55 +0200

    fix: :bug: next seo moved outside nextjs head tag

--- src/components/utils/ArMeta/ArMeta.tsx
+++ src/components/utils/ArMeta/ArMeta.tsx
@@ -11,43 +11,47 @@ export function ArMeta(props: ArMetaProps) {
   const { title, description, canonical, openGraph, ...rest } = props;
 
   return (
-    <Head>
-      <meta
-        charSet="UTF-8"
-        key="charset"
-      />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1"
-        key="viewport"
-      />
-      <link
-        rel="apple-touch-icon"
-        href={`${router.basePath}/apple-touch-icon.png`}
-        key="apple"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${router.basePath}/favicon-32x32.png`}
-        key="icon32"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${router.basePath}/favicon-16x16.png`}
-        key="icon16"
-      />
-      <link
-        rel="icon"
-        href={`${router.basePath}/favicon.png`}
-        key="favicon"
-      />
+    <>
+      <Head>
+        <meta
+          charSet="UTF-8"
+          key="charset"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+        <link
+          rel="apple-touch-icon"
+          href={`${router.basePath}/apple-touch-icon.png`}
+          key="apple"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${router.basePath}/favicon-32x32.png`}
+          key="icon32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${router.basePath}/favicon-16x16.png`}
+          key="icon16"
+        />
+        <link
+          rel="icon"
+          href={`${router.basePath}/favicon.png`}
+          key="favicon"
+        />
+      </Head>
+
       <NextSeo
         {...rest}
         title={title}
+        titleTemplate={`%s | ${APP_CONFIG.site_name}`}
         description={description}
         canonical={canonical}
         openGraph={{
@@ -60,6 +64,6 @@ export function ArMeta(props: ArMetaProps) {
           siteName: openGraph?.siteName || APP_CONFIG.site_name || '',
         }}
       />
-    </Head>
+    </>
   );
 }
