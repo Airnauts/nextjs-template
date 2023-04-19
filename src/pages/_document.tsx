90eb1952da8b9de524957ebba3adc50dad159b24
Damian Nowakowski <damian@airnauts.com>
2023-04-19 12:18:31 +0200

    feat: add cross-env

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
+import { APP_CONFIG } from 'config/app-config';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang={APP_CONFIG.locale}>
       <Head />
       <body>
         <Main />
